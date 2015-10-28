b4c352b73dade20bc29368b3dcff3c600bf6f7bb
Tommi Virtanen <tv@eagain.net>
2015-10-27 18:05:59 -0700

    Focus on uint16 and string for now, until codegen works
    Uint16 is a representative slot with endianness issues, String is a
    representative variable-length field. The rest will be restored once
    the churn slows down.

---
+++ schema/string.go
@@ -0,0 +1,5 @@
+package schema
+
+type String struct{}
+
+func (String) isField() {}
---
+++ schema/uint16.go
@@ -0,0 +1,7 @@
+package schema
+
+type Uint16 struct {
+}
+
+func (Uint16) isSlot()  {}
+func (Uint16) isField() {}
