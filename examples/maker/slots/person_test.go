b774185478e07d4eccde4655a030e9e71fc6114b
Tommi Virtanen <tv@eagain.net>
2016-01-12 19:38:41 -0800

    Hand-craft example Maker code, to sketch out the APIs

---
+++ examples/maker/slots/person_test.go
@@ -0,0 +1,20 @@
+package person_test
+
+//TODO go:generate go run gen.go
+
+import (
+	"fmt"
+
+	"chitin.io/chitin/examples/maker/slots"
+)
+
+func Example() {
+	m := person.NewPersonV1Maker()
+	m.SetAge(15)
+	m.SetSiblings(3)
+	buf := m.Bytes()
+	fmt.Printf("% 02x\n", buf)
+
+	// Output:
+	// 00 0f 00 03
+}
