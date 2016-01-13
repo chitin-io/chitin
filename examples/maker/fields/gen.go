4fc7331309323bf50cd7d7e44768b38e0f2182bc
Tommi Virtanen <tv@eagain.net>
2016-01-12 19:56:28 -0800

    Hand-craft example Maker code for fields

---
+++ examples/maker/fields/gen.go
@@ -0,0 +1,71 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"chitin.io/chitin/schema"
+)
+
+func run() error {
+	s := &schema.SchemaV1{
+		Messages: map[schema.Versioned]schema.Message{
+			{"Person", 2}: &schema.VarMessage{
+				WireFormat: 1,
+				Slots: []schema.Slot{
+					{
+						Name: "age",
+						Kind: schema.Uint16{},
+					},
+					{
+						Name: "siblings",
+						Kind: schema.Uint16{},
+					},
+				},
+				Fields: []schema.Field{
+					{
+						Name: "name",
+						Kind: schema.String{},
+					},
+				},
+			},
+		},
+	}
+
+	tmp, err := ioutil.TempFile(".", ".tmp-chitin-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	doClose := true
+	doRemove := true
+	defer func() {
+		if doClose {
+			_ = tmp.Close()
+		}
+		if doRemove {
+			_ = os.Remove(tmp.Name())
+		}
+	}()
+	if err := s.Generate(tmp, "person"); err != nil {
+		return fmt.Errorf("cannot generate wire format library: %v", err)
+	}
+	if err := tmp.Close(); err != nil {
+		return fmt.Errorf("error writing generated code: %v", err)
+	}
+	doClose = false
+	if err := os.Rename(tmp.Name(), "chitin.gen.go"); err != nil {
+		return fmt.Errorf("cannot rename temporary file: %v", err)
+	}
+	doRemove = false
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ examples/maker/fields/person_test.go
@@ -0,0 +1,21 @@
+package person_test
+
+//TODO go:generate go run gen.go
+
+import (
+	"fmt"
+
+	"chitin.io/chitin/examples/maker/fields"
+)
+
+func Example() {
+	m := person.NewPersonV2Maker()
+	m.SetAge(15)
+	m.SetSiblings(3)
+	m.SetName("Jane")
+	buf := m.Bytes()
+	fmt.Printf("%q\n", buf)
+
+	// Output:
+	// "\x00\x0f\x00\x03\x05Jane"
+}
