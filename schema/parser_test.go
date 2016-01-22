f56171f0a4eeece84434a4500af680270603f890
Tommi Virtanen <tv@eagain.net>
2016-01-21 18:45:06 -0800

    Test public API

--- schema/parser_test.go
+++ schema/parser_test.go
@@ -1,23 +1,23 @@
-package schema
+package schema_test
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"chitin.io/chitin/schema"
+)
 
 func TestFuzz1(t *testing.T) {
-	parser := makeParser()
-
-	parser.ParseString("chitin\tv1message 0\tv0{slots{}")
-
+	input := strings.NewReader("chitin\tv1message 0\tv0{slots{}")
+	_, _ = schema.Parse(input)
 }
 
 func TestFuzz2(t *testing.T) {
-	parser := makeParser()
-
-	parser.ParseString("chitin\tv1envelope\t0{}")
+	input := strings.NewReader("chitin\tv1envelope\t0{}")
+	_, _ = schema.Parse(input)
 }
 
 func TestFuzz3(t *testing.T) {
-	parser := makeParser()
-
-	parser.ParseString("chitin\nv1message\n0\nv0{slots{0\nstring")
-
+	input := strings.NewReader("chitin\nv1message\n0\nv0{slots{0\nstring")
+	_, _ = schema.Parse(input)
 }
